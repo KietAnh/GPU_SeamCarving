a0ee69eae16fae178e25d5e667e182201e9c6d15
Kiet Huynh <huynhkietno1@gmail.com>
2021-01-09 15:20:24 +0700

    initial commit

---
+++ SeamCarving.cu
@@ -0,0 +1,15 @@
+// Last update: 09/01/2021
+#include <stdio.h>
+#include <stdint.h>
+
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess)\
+    {\
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+        fprintf(stderr, "code: %d, reason: %s\n", error,\
+                cudaGetErrorString(error));\
+        exit(1);\
+    }\
+}
